77b945c3fe4c49d612e93a74a7ae813edc520077
Neil Turner <travisneilturner@gmail.com>
2024-03-23 08:43:27 -0700

    use node ids in dict to save space

--- hnsw.py
+++ hnsw.py
@@ -14,7 +14,7 @@ def hamming_distance_numpy(a: np.ndarray, b: np.ndarray) -> int:
     return count
 
 class HNSWNode(BaseModel):
-    neighbors: Dict[int, Set[HNSWNode]]
+    neighbors: Dict[int, Set[int]]
     point: List[int]
     id: int
 
@@ -26,11 +26,12 @@ class HNSWNode(BaseModel):
 
 class HNSW(BaseModel):
     max_layers: int
-    layers: Dict[int, Set[HNSWNode]] = {}
+    layers: Dict[int, Set[int]] = {}
+    nodes: Dict[int, HNSWNode] = {}
     num_nodes: int = 0
 
     def _insert(self, point:List[int]):
-        node_id = self.num_nodes + 1
+        node_id = self.num_nodes
         self.num_nodes += 1
 
         node = HNSWNode(neighbors={}, point=point, id=node_id)
@@ -43,12 +44,13 @@ class HNSW(BaseModel):
                 node.neighbors[layer] = set()
             if layer not in self.layers:
                 self.layers[layer] = set()
-            self.layers[layer].add(node)
+            self.layers[layer].add(node.id)
+            self.nodes[node.id] = node
             for nearest in self._find_nearest_k_in_layer(node, layer, 10):
                 if layer not in nearest.neighbors:
                     nearest.neighbors[layer] = set()
-                nearest.neighbors[layer].add(node)
-                node.neighbors[layer].add(nearest)
+                nearest.neighbors[layer].add(node.id)
+                node.neighbors[layer].add(nearest.id)
 
     def _decide_layer_for_new_node(self):
         r = random.random()  # Generate a random number between 0 and 1
@@ -60,28 +62,28 @@ class HNSW(BaseModel):
 
     def _find_nearest_k_in_layer(self, origin: HNSWNode, layer: int, k: int = 1) -> List[HNSWNode]:
        # Store (distance, neighbor) tuples in a list
-        distance_neighbor_pairs = []
+        distance_neighbor_id_pairs = []
 
         # Add origin's neighbors in the layer to the list
-        for neighbor in origin.neighbors.get(layer, []):
-            distance = hamming_distance_numpy(np.array(origin.point), np.array(neighbor.point))
-            distance_neighbor_pairs.append((distance, neighbor))
+        for neighbor_id in origin.neighbors.get(layer, []):
+            distance = hamming_distance_numpy(np.array(origin.point), np.array(self.nodes[neighbor_id].point))
+            distance_neighbor_id_pairs.append((distance, neighbor_id))
 
-        all_nodes_in_layer = set(self.layers[layer])
-        remaining_neighbors = all_nodes_in_layer - set(origin.neighbors.get(layer, []))
+        all_node_ids_in_layer = set(self.layers[layer])
+        remaining_neighbor_ids = all_node_ids_in_layer - set(origin.neighbors.get(layer, []))
         
         # Add remaining neighbors to the list
-        for remainingNeighbor in remaining_neighbors:
-            if origin.__eq__(remainingNeighbor):
+        for remaining_neighbor_id in remaining_neighbor_ids:
+            if self.nodes[origin.id].__eq__(self.nodes[remaining_neighbor_id]):
                 continue
-            distance = hamming_distance_numpy(np.array(origin.point), np.array(remainingNeighbor.point))
-            distance_neighbor_pairs.append((distance, remainingNeighbor))
+            distance = hamming_distance_numpy(np.array(origin.point), np.array(self.nodes[remaining_neighbor_id].point))
+            distance_neighbor_id_pairs.append((distance, remaining_neighbor_id))
 
         # Sort the combined list by distance
-        distance_neighbor_pairs.sort(key=lambda x: x[0])
+        distance_neighbor_id_pairs.sort(key=lambda x: x[0])
 
         # Take the first k elements after sorting
-        nearest_neighbors = [neighbor for _, neighbor in distance_neighbor_pairs[:k]]
+        nearest_neighbors = [self.nodes[neighbor_id] for _, neighbor_id in distance_neighbor_id_pairs[:k]]
 
         return nearest_neighbors
 
@@ -89,31 +91,31 @@ class HNSW(BaseModel):
         nearest_neighbor_for_layer = (None, float("inf"))
         for layer in range(self.max_layers, -1, -1):
             if nearest_neighbor_for_layer[0] is None:
-                searchNodes = self.layers.get(layer, []) 
+                search_ids = self.layers.get(layer, []) 
             else:
-                searchNodes = list(nearest_neighbor_for_layer[0].neighbors.get(layer, []))
+                search_ids = list(self.nodes[nearest_neighbor_for_layer[0]].neighbors.get(layer, []))
              # Add origin's neighbors in the layer to the list
-            for node in searchNodes:
-                distance = hamming_distance_numpy(np.array(node.point), np.array(point))
+            for id in search_ids:
+                distance = hamming_distance_numpy(np.array(self.nodes[id].point), np.array(point))
                 if distance < nearest_neighbor_for_layer[1]:
-                    nearest_neighbor_for_layer = (node, distance)
-        return nearest_neighbor_for_layer[0].point
+                    nearest_neighbor_for_layer = (id, distance)
+        return self.nodes[nearest_neighbor_for_layer[0]].point
 
 def main():
     data = np.random.uniform(low=-1, high=1, size=(100, 128))
 
     hnsw = HNSW(max_layers=10) 
-    bemb = np.where(data < 0, 0, 1).astype(np.uint8)
-    bemb = np.packbits(bemb).reshape(bemb.shape[0], -1)
+    binary_vecs = np.where(data < 0, 0, 1).astype(np.uint8)
+    binary_vecs = np.packbits(binary_vecs).reshape(binary_vecs.shape[0], -1)
 
     # insert each row in bemb
-    for row in bemb:
+    for row in binary_vecs:
         hnsw._insert(row.tolist())
     
     query = np.random.uniform(low=-1, high=1, size=(1, 128))
-    bemb = np.where(query < 0, 0, 1).astype(np.uint8)
-    bemb = np.packbits(bemb).reshape(bemb.shape[0], -1)
-    found = hnsw.search(bemb.reshape(-1).tolist())
+    binary_vec = np.where(query < 0, 0, 1).astype(np.uint8)
+    binary_vec = np.packbits(binary_vec).reshape(binary_vec.shape[0], -1)
+    found = hnsw.search(binary_vec.reshape(-1).tolist())
 
     print(found)
 
